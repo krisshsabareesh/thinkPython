fcb10c32ed8fea94b8f59a99205e8000a1e9570d
Sabareesh Krishnan <krisshsabareesh@gmail.com>
2016-07-21 10:02:21 +0530

    One file updated

--- 5conditionalsAndRecursion/input.py
+++ 5conditionalsAndRecursion/input.py
@@ -4,6 +4,7 @@ mob=int(raw_input('what is your mobile no:'))
 print 'Thank you, Your mobile no. is: '+str(mob)
 cod=(mob+234)/8
 print 'Your code is: '+str(cod)
-prompt='What....is the airspeed velocity of an unladen swallow?\n'
+prompt='What... is the acceleration due to gravity..?\n'
 speed=raw_input(prompt)
-print speed
+print speed+'(ans: 9.8 m/s^2)
+
